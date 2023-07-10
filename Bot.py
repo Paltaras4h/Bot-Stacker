8572b7dce25884259a81dd7148ff1267aac01dd7
s25140 <s25140@pjwstk.edu.pl>
2023-07-10 14:30:42 +0200

    + user secrets, test discord bot first run

--- Bot.py
+++ Bot.py
@@ -1,4 +1,48 @@
 import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
 
-print("Hello World!")
-print("Admina na mylo")
+# Load environment variables from the .env file
+load_dotenv('secrets.env')
+
+# Access secrets using os.environ
+bot_token = os.environ.get('BOT_TOKEN')
+
+
+# Create an instance of the bot
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+# Event: Bot is ready and connected to the server
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+    print(f'Bot ID: {bot.user.id}')
+    print('------')
+
+# Command: !htolox
+@bot.command()
+async def htolox(ctx):
+    await ctx.send('zeniya')
+
+# Event: Message is received
+@bot.event
+async def on_message(message):
+    # Ignore if the message is from the bot itself
+    if message.author == bot.user:
+        return
+
+    # Respond to a specific message content
+    if message.content.lower() == 'ping':
+        await message.channel.send('Pong!')
+
+    await bot.process_commands(message)
+
+# Event: Bot joins a server
+@bot.event
+async def on_guild_join(guild):
+    print(f'Bot has joined {guild.name} (ID: {guild.id})')
+print(bot_token)
+# Run the bot using your bot token
+bot.run(bot_token)
+
