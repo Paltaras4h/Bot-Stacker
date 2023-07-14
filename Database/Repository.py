989229c7a747b8fc34148328749906352d5ce805
Paltaras4h <pasha.khrapko@gmail.com>
2023-07-14 15:57:14 +0200

    time handling integrated

--- Database/Repository.py
+++ Database/Repository.py
@@ -1,10 +1,21 @@
 import Database.StackDatabase as db
 from Models.User import User
 from Models.Stack import Stack
+from datetime import datetime, timedelta
+
+def normalize_time(time):
+    now = datetime.now()
+    time = now.replace(hour=time.hour, minute=time.minute)
+    if time.replace(second=0, microsecond=0) < now.replace(second=0, microsecond=0):
+        time += timedelta(days=1)
+    return time
 
 def set_user_time_frame(user, time_from, time_to, UTC=None):
-    user.default_time_from = time_from
-    user.default_time_to = time_to
+    if type(time_from) != datetime or type(time_to) != datetime:
+        raise TypeError("Об'єкт Datetime передавай в time_from й time_to, уася")
+
+    user.default_time_from = normalize_time(time_from)
+    user.default_time_to = normalize_time(time_to)
     if UTC:
         user.UTC = UTC
     db.update_user(user)
@@ -18,16 +29,18 @@ def set_stack_time_frame(stack, time_from, time_to):
 
 def get_user(id, name):
     """creates if does not exist"""
-    user = db.select_user(id)
+    cnx = db.connect_to_data_base(False)
+    user = db.select_user(id, cnx=cnx)
     if user:
-        _user = User(id, name, user[2], user[3], user[4])
-        if _user.name != user[1]:
-            db.update_user(_user)
-        return _user
+        _user = User(id, name, normalize_time(user[2]), normalize_time(user[3]), user[4])
+        db.update_user(_user, cnx=cnx)
     else:
         _user = User(id, name)
-        db.insert_user(_user)
-        return _user
+        db.insert_user(_user, cnx=cnx)
+
+    cnx.commit()
+    cnx.close()
+    return _user
 
 def create_stack(user):
     """----------------------------------\n
